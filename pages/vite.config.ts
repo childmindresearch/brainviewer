138148b014e79e636ae4c63d7b09fed67335e823
Florian Rupprecht <floruppr@gmail.com>
2023-07-21 12:40:45 -0400

    Fix things

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/brainviewer/'
-})
+    plugins: [svelte()],
+    base: "/brainviewer/",
+    server: {
+        fs: {
+            allow: ["../../"],
+        },
+    },
+});
